c3cd9a4788471bd623868031036c9e77901d07b8
builtbyjosh <joshuacollins912@gmail.com>
2023-09-12 10:42:01 -0500

    playing with variable in views

--- new_site/my_app/urls.py
+++ new_site/my_app/urls.py
@@ -1,6 +1,4 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [
-    path("", views.example_view),
-]
+urlpatterns = [path("", views.example_view), path("variable/", views.variable_view)]
